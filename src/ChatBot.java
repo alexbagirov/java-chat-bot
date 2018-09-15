c34c80297b67beef50e161a65921e06740b1bd4e
alexbagirov <a.d.bagirov@gmail.com>
2018-09-15 20:29:34 +0500

    Добавлены распознавание игровых команд и загототовка для бин. поиска

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,15 +1,36 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+enum NumberIs { BIGGER, SMALLER }
 
-public class ChatBot {
-	
-	public final static Map<String,String> answers = new HashMap<String,String>();
-	
-	public void Answer(String methodName) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
-	{
-		Method method = this.getClass().getDeclaredMethod(methodName);
-        method.invoke(this);
+class ChatBot {
+	String Answer(String request) throws SecurityException, IllegalArgumentException {
+		switch (request) {
+            case "старт":
+                return String.format("Игра началась.\nПервое число: %d?", GuessNumber());
+            case "о игре":
+                return "Когда ты загадаешь число и начнёшь игру, я буду предлагать разные варианты.\nЕсли твоё число" +
+                        "больше моего, ответь \"больше\", если меньше - \"меньше\". Если было названо верное число, " +
+                        "напиши \"угадал\".\nСтарт игры - команда \"старт\", остановка - \"стоп\".";
+            case "стоп":
+                return "Игра закончена. Для продолжения введи следующую команду.";
+            case "больше":
+            case ">":
+                UpdateBorders(NumberIs.BIGGER);
+                return String.format("Может, это %d?", GuessNumber());
+            case "меньше":
+            case "<":
+                UpdateBorders(NumberIs.SMALLER);
+                return String.format("Может, это %d?", GuessNumber());
+            case "угадал":
+                return "Ура! Игра закончена. Для продолжения введи следующую команду.";
+			default:
+			    return "Команда не распознана. Попробуй ещё раз.";
+		}
 	}
+
+	private int GuessNumber() {
+	    throw new UnsupportedOperationException();
+    }
+
+    private void UpdateBorders(NumberIs answer) {
+	    throw new UnsupportedOperationException();
+    }
 }
